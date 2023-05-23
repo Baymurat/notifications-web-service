d072c6fd87628e8ebab29ae7e5491e9f9ceab3a2
Baymurat <baymurat.orazov@mail.ru>
2023-05-23 17:39:56 +0500

    refactor: use express for websocket

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,9 @@ import express, { Request, Response } from "express"
 import dotenv from 'dotenv'
 import { WebSocketServer, type WebSocket } from 'ws'
 import uuid from 'node-uuid'
+import http from 'http'
 
 const whatsAppClient = require("@green-api/whatsapp-api-client")
-
-dotenv.config()
-const port = process.env.PORT ?? 6001
-const webhookUrl = process.env.WEBHOOK_URL
-
-const app = express();
-app.use(express.json());
-
-app.get('/', (req: Request, res: Response) => {
-  res.status(200).json('Hello World!')
-})
-
-const wss = new WebSocketServer({ port: 4444 })
 
 interface IClient {
   idInstance: string
@@ -24,50 +12,63 @@ interface IClient {
   ws: WebSocket
 }
 
-const clients: IClient[] = []
+dotenv.config()
 
-wss.on('connection', (ws) => {
-  const clientId = uuid.v4()
+const port = process.env.PORT ?? 6001
+const webhookUrl = process.env.WEB_HOOK_URL
 
-  ws.addEventListener('message', async ({ data }) => {
-    const { command, idInstance, apiTokenInstance } = JSON.parse(data as string)
-    console.log(command, idInstance, apiTokenInstance);
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
+const clients: IClient[] = [];
 
-    if (command === 'init') {
-      try {
-        const response = await fetch(
-          `https://api.green-api.com/waInstance${idInstance}/SetSettings/${apiTokenInstance}`,
-          {
-            method: 'POST',
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ webhookUrl })
+app.use(express.json());
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json('Hello World!')
+});
+
+const main = async () => {
+  wss.on('connection', (ws) => {
+    const clientId = uuid.v4()
+
+    ws.addEventListener('message', async ({ data }) => {
+      const { command, idInstance, apiTokenInstance } = JSON.parse(data as string)
+      console.log(command, idInstance, apiTokenInstance);
+
+      if (command === 'init') {
+        try {
+          const response = await fetch(
+            `https://api.green-api.com/waInstance${idInstance}/SetSettings/${apiTokenInstance}`,
+            {
+              method: 'POST',
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({ webhookUrl })
+            }
+          )
+
+          const body = await response.json()
+          if (body.saveSettings) {
+            clients.push({ idInstance, ws, id: clientId })
           }
-        )
-
-        const body = await response.json()
-        if (body.saveSettings) {
-          clients.push({ idInstance, ws, id: clientId })
+        } catch (err) {
+          console.error(err)
         }
-      } catch (err) {
-        console.error(err)
       }
-    }
-  })
+    })
 
-  ws.on('close', () => {
-    const clientIndex = clients.findIndex((client) => client.id === clientId)
-    if (clientIndex !== -1) {
-      clients.splice(clientIndex, 1)
-    }
-  })
-})
+    ws.on('close', () => {
+      const clientIndex = clients.findIndex((client) => client.id === clientId)
+      if (clientIndex !== -1) {
+        clients.splice(clientIndex, 1)
+      }
+    })
+  });
+
   // Receive webhook
-  ;
-(async () => {
   try {
-    const webHookAPI = whatsAppClient.webhookAPI(app, '/webhook')
+    const webHookAPI = whatsAppClient.webhookAPI(app, '/green-api/webhook')
 
     webHookAPI.onIncomingMessageText((data: any, idInstance: any, idMessage: any, sender: any, typeMessage: any, textMessage: any) => {
       const client = clients.find((client) => Number(client.idInstance) === idInstance)
@@ -79,8 +80,10 @@ wss.on('connection', (ws) => {
     console.error(error);
     process.exit(1);
   }
-})();
 
-app.listen(port, () => {
-  console.log(`Application started on port ${port}`);
-})
+  server.listen(port, () => {
+    console.log(`Application started on port ${port}`);
+  })
+}
+
+main()
